60339a115dda8f6cc5cc4fd00c7174200d299099
Victor <victorca380@gmail.com>
2024-11-30 19:31:04 +0100

    Fix astro.config.mjs and package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,40 +6,52 @@ import vercel from "@astrojs/vercel/serverless";
 import partytown from "@astrojs/partytown";
 
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind(),
-    partytown(),
-    icon({
-      include: {
-        mdi: ["*"],
-      },
-    })
-  ],
-  output: "server",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-    speedInsights: {
-      enabled: true,
-    },
-    imageService: true,
-    imagesConfig: {
-      sizes: [640, 750, 828, 1080, 1200, 1920],
-      domains: [],
-      minimumCacheTTL: 60,
-    },
-  }),
-  vite: {
-    build: {
-      minify: false, // Changed from terser to false temporarily
-    },
-    optimizeDeps: {
-      exclude: ['@astrojs/vercel', '@vercel/node']
-    },
-    ssr: {
-      noExternal: ['react-icons', '@astrojs/*']
-    }
-  }
+ integrations: [
+   react({
+     include: ['@astrojs/*']
+   }),
+   tailwind(),
+   partytown({
+     config: {
+       forward: ['dataLayer.push']
+     }
+   }),
+   icon({
+     include: {
+       mdi: ["*"],
+     }
+   })
+ ],
+ output: "server",
+ adapter: vercel({
+   webAnalytics: {
+     enabled: true,
+   },
+   speedInsights: {
+     enabled: true,
+   },
+   imageService: true,
+   imagesConfig: {
+     sizes: [640, 750, 828, 1080, 1200, 1920],
+     domains: [],
+     minimumCacheTTL: 60,
+   }
+ }),
+ vite: {
+   build: {
+     target: 'esnext',
+     modulePreload: {
+       polyfill: false
+     }
+   },
+   optimizeDeps: {
+     exclude: ['@astrojs/vercel', '@vercel/node'],
+     esbuildOptions: {
+       target: 'esnext'
+     }
+   },
+   ssr: {
+     noExternal: ['react-icons', '@astrojs/*']
+   }
+ }
 });
